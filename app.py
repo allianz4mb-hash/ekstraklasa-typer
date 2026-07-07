e5b5799e7882e665ea9ef141e729f3261a28a8e8
allianz4mb-hash <allianz4.mb@gmail.com>
2026-07-07 18:36:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,48 @@
 import streamlit as st
 from supabase import create_client, Client
-from bs4 import BeautifulSoup
-import requests
 
-st.set_page_config(page_title="Typer Mundialu", page_icon="⚽")
-
-# Konfiguracja Supabase
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(url, key)
 
-st.title("⚽ Typer Mundialu")
-
-# Logowanie
-if 'nick' not in st.session_state:
-    st.session_state.nick = ''
+if 'nick' not in st.session_state: st.session_state.nick = ''
 
 if st.session_state.nick == '':
-    wpisany_nick = st.text_input("Podaj swój nick:")
+    wpisany_nick = st.text_input("Nick:")
     wpisane_haslo = st.text_input("Hasło:", type="password")
     if st.button("Wejdź"):
-        # Logika sprawdzania użytkownika...
-        response = supabase.table('gracze').select('*').eq('nick', wpisany_nick).execute()
-        if not response.data:
+        res = supabase.table('gracze').select('*').eq('nick', wpisany_nick).execute()
+        if not res.data:
             supabase.table('gracze').insert({'nick': wpisany_nick, 'haslo': wpisane_haslo}).execute()
         st.session_state.nick = wpisany_nick
         st.rerun()
 else:
-    st.success(f"Witaj, {st.session_state.nick}!")
-    
-    # Zakładki
+    st.title(f"Witaj, {st.session_state.nick}!")
     tab1, tab2, tab3 = st.tabs(["Typer", "Ranking", "Admin"])
     
     with tab1:
         st.subheader("Obstaw mecze")
-        # Tu będziemy wyświetlać mecze z bazy
-        
+        mecze = supabase.table("mecze").select("*").execute().data
+        for m in mecze:
+            col1, col2, col3 = st.columns([3, 1, 3])
+            col1.write(f"{m['gospodarze']} vs {m['goscie']}")
+            g = col2.number_input("G", 0, 10, key=f"g_{m['id']}")
+            go = col2.number_input("Go", 0, 10, key=f"go_{m['id']}")
+            if col2.button("Zapisz", key=f"btn_{m['id']}"):
+                supabase.table("typy").upsert({
+                    "nick": st.session_state.nick,
+                    "mecz_id": m['id'],
+                    "typ_gospodarze": g,
+                    "typ_goscie": go
+                }).execute()
+                st.success("Zapisano!")
+
     with tab2:
-        st.subheader("Tabela wyników")
-        # Tu będzie ranking
-        
+        st.subheader("Ranking")
+        gracze = supabase.table("gracze").select("nick, punkty").order("punkty", desc=True).execute().data
+        st.table(gracze)
+
     with tab3:
-        if st.button("Odśwież dane z internetu"):
-            st.info("Tu będzie działał nasz scraper!")
-    
-    if st.button("Wyloguj"):
-        st.session_state.nick = ''
-        st.rerun()
+        if st.button("Wyloguj"):
+            st.session_state.nick = ''
+            st.rerun()
