af6f3aebe3179d9857df39bde0f953d856afb8b7
allianz4mb-hash <allianz4.mb@gmail.com>
2026-07-07 17:43:53 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Typer Ekstraklasy", page_icon="⚽")
+
+st.title("⚽ Typer Ekstraklasy")
+st.write("Witaj! Tutaj wkrótce pojawi się nasza aplikacja do obstawiania meczów ze znajomymi.")
