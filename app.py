de2e96d872db5fd00d415f2a6d06ce8f99b204b8
allianz4mb-hash <allianz4.mb@gmail.com>
2026-07-08 21:51:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ def get_last_sync_time():
 def render_header():
     c1, c2 = st.columns([0.1, 0.9])
     with c1:
-        # Próba załadowania grafiki z alternatywnego źródła
-        st.image("https://icons.iconarchive.com/icons/google/noto-emoji-activities/512/52775-soccer-ball-icon.png", width=60)
+        # Logo z linku od Ciebie
+        st.image("https://www.footylogos.com/logos/fifa-world-cup-2026", width=60)
     with c2:
         st.title("Typer Mundialu")
 
