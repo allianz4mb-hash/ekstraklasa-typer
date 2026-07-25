cd6b65921655fcba4374999b010dc953ceed7ba1
allianz4mb-hash <allianz4.mb@gmail.com>
2026-07-25 12:02:43 +0300

    Update matryca.py

--- views/matryca.py
+++ views/matryca.py
@@ -4,6 +4,12 @@ import streamlit as st
 import utils
 
 
+def formatuj_nazwe_kolejki(kolejka_raw):
+  """Zamienia np. 'Regular Season - 1' na '1. Kolejka PKO BP Ekstraklasy'"""
+  num = utils.wyciagnij_numer_kolejki(kolejka_raw)
+  return f"{num}. Kolejka PKO BP Ekstraklasy"
+
+
 def render_matryca(wszystkie_mecze, zalogowany_gracz):
   st.header("👁️ Podgląd Typów Rywali / Matryca Kolejki")
 
@@ -11,16 +17,24 @@ def render_matryca(wszystkie_mecze, zalogowany_gracz):
     st.info("Brak meczów w bazie.")
     return
 
-  kolejki = sorted(
+  # Wyciągamy i sortujemy unikalne surowe nazwy kolejek
+  kolejki_raw = sorted(
       list(set(m["kolejka"] for m in wszystkie_mecze)),
       key=utils.wyciagnij_numer_kolejki,
   )
-  wybrana_kolejka = st.selectbox(
-      "Wybierz kolejkę do podglądu:", kolejki, key="matryca_kolejka"
+
+  # Tworzymy mapę z ładną nazwą dla selectboxa
+  kolejki_mapa = {k: formatuj_nazwe_kolejki(k) for k in kolejki_raw}
+
+  wybrana_kolejka_raw = st.selectbox(
+      "Wybierz kolejkę do podglądu:",
+      options=kolejki_raw,
+      format_func=lambda k: kolejki_mapa[k],
+      key="matryca_kolejka",
   )
 
   mecze_w_kolejce = [
-      m for m in wszystkie_mecze if m["kolejka"] == wybrana_kolejka
+      m for m in wszystkie_mecze if m["kolejka"] == wybrana_kolejka_raw
   ]
   wszystkie_typy = database.pobierz_wszystkie_typy()
   lista_graczy = database.pobierz_liste_graczy()
